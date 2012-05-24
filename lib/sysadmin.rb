8c44dcf3f1f1e331a8d823f8e1da1f0d53f6649c
id774 <idnanashi@gmail.com>
2012-05-24 18:39:46 +0900

    5 Version bump to 0.0.2

--- lib/sysadmin.rb
+++ lib/sysadmin.rb
@@ -1,44 +1,19 @@
 #!/usr/bin/env ruby
 # Name::      Sysadmin::Ruby
 # Author::    774 <http://id774.net>
-# Version::   0.0.1
+# Version::   0.0.2
 # Created::   Mar 23, 2012
-# Updated::   Mar 23, 2012
+# Updated::   Mar 24, 2012
 # Copyright:: 774 Copyright (c) 2012
 # License::   Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3.0.
 
 module Sysadmin
-  require 'sysadmin/file_string'
-
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
   USER_DIR = "/."
+  ROOT_DIR = File.expand_path("..", File.dirname(__FILE__))
+  $:.unshift ROOT_DIR
+  $:.unshift ROOT_DIR + '/lib'
+  $:.unshift ROOT_DIR + '/lib/sysadmin'
 
-  class << self
-    attr_accessor :root_dir
-
-    def lib_dir
-      File.join(@root_dir, "lib")
-    end
-
-    def config_dir
-      File.join(@root_dir, "config")
-    end
-
-    def user_dir
-      @user_dir
-    end
-
-    def user_dir=(_user_dir)
-      if ENV["SYSADMIN_RUBY_ENV"] == "test" && !(_user_dir.nil?)
-        @user_dir = _user_dir 
-      else
-        @user_dir = File.expand_path("~/") + USER_DIR
-      end
-    end
-
-    def user_plugins_dir
-      File.join(@user_dir, "lib")
-    end
-  end
-
+  require 'file_string'
 end
--- lib/sysadmin/file_string.rb
+++ lib/sysadmin/file_string.rb
@@ -1,7 +1,7 @@
 # Name::      Sysadmin::FileString
 # Author::    774 <http://id774.net>
 # Created::   Mar 23, 2012
-# Updated::   Mar 23, 2012
+# Updated::   Mar 24, 2012
 # Copyright:: 774 Copyright (c) 2012
 # License::   Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3.0.
 
@@ -36,9 +36,9 @@ module Sysadmin
     end
 
     def self.replace(file, src, out)
-      open(file,"r+") { |f|
+      open(file, "r+") { |f|
         f.flock(File::LOCK_EX)
-        replaceFile(f,src,out)
+        replaceFile(f, src, out)
         f.truncate(f.tell)
       }
     end
@@ -48,7 +48,7 @@ module Sysadmin
       IO.foreach(file) { |line|
         out << line unless line.include?(str)
       }
-      open(file,"w") { |f|
+      open(file, "w") { |f|
         f.write out
       }
     end
