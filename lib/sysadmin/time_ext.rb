6533620f73a0940720fabc7af9ca5333ede115f5
id774 <idnanashi@gmail.com>
2012-09-27 14:23:11 +0900

    26 Fix typo

--- lib/sysadmin/time_ext.rb
+++ lib/sysadmin/time_ext.rb
@@ -5,7 +5,7 @@
 # Copyright:: 774 Copyright (c) 2012
 # License::   Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3.0.
 
-require "time"
+require 'time'
 
 module Sysadmin
   module TimeExtension
