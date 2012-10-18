895f316cd31f10f20dd24142af3307b20eb1b4e7
id774 <idnanashi@gmail.com>
2012-10-18 13:29:53 +0900

    29 Renew test data

--- spec/lib/sysadmin/dir_ext_spec.rb
+++ spec/lib/sysadmin/dir_ext_spec.rb
@@ -6,14 +6,14 @@ describe Sysadmin::DirExtension, 'Dir クラス拡張' do
   context 'で filelist メソッドを呼ぶ場合' do
     describe 'ディレクトリ名を指定すると' do
       it "ディレクトリ内のファイル一覧が返却される" do
-        test_dir = File.expand_path("../../test_dir", File.dirname(__FILE__))
+        test_dir = File.expand_path("../../test_dir/a", File.dirname(__FILE__))
         Dir.filelist(test_dir).should have(2).items
       end
     end
 
     describe 'ファイル名を指定すると' do
       it "ファイル名が返却される" do
-        test_dir = File.expand_path("../../test_dir/file2", File.dirname(__FILE__))
+        test_dir = File.expand_path("../../test_dir/a/d.txt", File.dirname(__FILE__))
         Dir.filelist(test_dir).should have(1).items
       end
     end
@@ -21,20 +21,20 @@ describe Sysadmin::DirExtension, 'Dir クラス拡張' do
     describe 'サブディレクトリを true にしてディレクトリを指定すると' do
       it "サブディレクトリ内のファイルも返却される" do
         test_dir = File.expand_path("../../test_dir", File.dirname(__FILE__))
-        Dir.filelist(test_dir, sub_directory = true).should have(6).items
+        Dir.filelist(test_dir, sub_directory = true).should have(9).items
       end
     end
 
     describe 'サブディレクトリを false にしてディレクトリを指定すると' do
       it "ディレクトリ内のファイル一覧が返却される (サブディレクトリは見ない)" do
-        test_dir = File.expand_path("../../test_dir", File.dirname(__FILE__))
+        test_dir = File.expand_path("../../test_dir/a", File.dirname(__FILE__))
         Dir.filelist(test_dir, sub_directory = false).should have(2).items
       end
     end
 
     describe 'サブディレクトリを true にしてファイル名を指定しても' do
       it "ファイル名が返却される (サブディレクトリは見ない)" do
-        test_dir = File.expand_path("../../test_dir/file2", File.dirname(__FILE__))
+        test_dir = File.expand_path("../../test_dir/a/d.txt", File.dirname(__FILE__))
         Dir.filelist(test_dir).should have(1).items
       end
     end
--- spec/lib/sysadmin/directory_spec.rb
+++ spec/lib/sysadmin/directory_spec.rb
@@ -7,49 +7,49 @@ describe Sysadmin::Directory, 'Directory クラス' do
     describe 'ディレクトリ名を指定すると' do
       it "ディレクトリ内のファイル一覧が返却される" do
         test_dir = File.expand_path("../../test_dir", File.dirname(__FILE__))
-        Sysadmin::Directory.new(test_dir).map{|f|f}.should have(7).items
+        Sysadmin::Directory.new(test_dir).map{|f|f}.should have(9).items
       end
     end
 
     describe 'ディレクトリ名を指定すると' do
       it "ディレクトリ内のファイル一覧が返却される" do
-        test_dir = File.expand_path("../../test_dir/dir2", File.dirname(__FILE__))
-        Sysadmin::Directory.new(test_dir).map{|f|f}.should have(2).items
+        test_dir = File.expand_path("../../test_dir/a", File.dirname(__FILE__))
+        Sysadmin::Directory.new(test_dir).map{|f|f}.should have(4).items
       end
     end
 
     describe 'ディレクトリ名を指定すると' do
       it "ディレクトリ内のファイル一覧が返却される" do
-        test_dir = File.expand_path("../../test_dir/dir3", File.dirname(__FILE__))
-        Sysadmin::Directory.new(test_dir).map{|f|f}.should have(1).item
+        test_dir = File.expand_path("../../test_dir/a/g", File.dirname(__FILE__))
+        Sysadmin::Directory.new(test_dir).map{|f|f}.should have(2).item
       end
     end
 
     describe 'ディレクトリ名を指定すると' do
       it "ディレクトリ内のファイル一覧が返却される" do
-        test_dir = File.expand_path("../../test_dir/dir6", File.dirname(__FILE__))
-        Sysadmin::Directory.new(test_dir).map{|f|f}.should have(1).item
+        test_dir = File.expand_path("../../test_dir/b", File.dirname(__FILE__))
+        Sysadmin::Directory.new(test_dir).map{|f|f}.should have(2).item
       end
     end
 
     describe 'ディレクトリ名を指定し grep すると' do
       it "ディレクトリ内の grep に適合したファイル一覧が返却される" do
         test_dir = File.expand_path("../../test_dir", File.dirname(__FILE__))
-        Sysadmin::Directory.new(test_dir).grep(/\.file7$/).should have(1).item
+        Sysadmin::Directory.new(test_dir).grep(/\.txt/).should have(9).item
       end
     end
 
     describe 'ディレクトリ名を指定し grep すると' do
       it "ディレクトリ内の grep に適合したファイル一覧が返却される" do
         test_dir = File.expand_path("../../test_dir", File.dirname(__FILE__))
-        Sysadmin::Directory.new(test_dir).grep(/\.file/).should have(3).items
+        Sysadmin::Directory.new(test_dir).grep(/d\.txt/).should have(1).items
       end
     end
 
     describe 'ディレクトリ名を指定し grep して take すると' do
       it "ディレクトリ内の grep に適合し take した数だけファイル一覧が返却される" do
         test_dir = File.expand_path("../../test_dir", File.dirname(__FILE__))
-        Sysadmin::Directory.new(test_dir).grep(/\.file/).take(2).should have(2).items
+        Sysadmin::Directory.new(test_dir).grep(/\.txt/).take(3).should have(3).items
       end
     end
 
