7d07c5ef3195cb128349e654fe4e690311dde56d
id774 <idnanashi@gmail.com>
2013-10-08 16:06:09 +0900

    43 Version bump to 0.1.5

--- lib/sysadmin.rb
+++ lib/sysadmin.rb
@@ -2,12 +2,12 @@
 # Name::      Sysadmin
 # Author::    774 <http://id774.net>
 # Created::   Mar 23, 2012
-# Updated::   Aug 20, 2013
+# Updated::   Oct 08, 2013
 # Copyright:: 774 Copyright (c) 2012
 # License::   Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3.0.
 
 module Sysadmin
-  VERSION = "0.1.4"
+  VERSION = "0.1.5"
   require File.dirname(__FILE__) +  "/sysadmin/util"
   require File.dirname(__FILE__) +  "/sysadmin/array_ext"
   require File.dirname(__FILE__) +  "/sysadmin/file_ext"
--- spec/lib/sysadmin_spec.rb
+++ spec/lib/sysadmin_spec.rb
@@ -5,7 +5,7 @@ require File.dirname(__FILE__) + '/../spec_helper'
 describe Sysadmin, 'sysadmin' do
   context 'のバージョンを参照した場合' do
     it "バージョンが正しく表示される" do
-      expect = '0.1.4'
+      expect = '0.1.5'
       Sysadmin.const_get(:VERSION).should be_true
       Sysadmin.const_get(:VERSION).should == expect
     end
