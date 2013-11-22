83d0b1310ee51c4180ffd8b46717b67748fe2009
id774 <idnanashi@gmail.com>
2013-11-22 10:37:50 +0900

    45 Refactoring spec for dir_ext

--- spec/lib/sysadmin/dir_ext_spec.rb
+++ spec/lib/sysadmin/dir_ext_spec.rb
@@ -2,42 +2,48 @@
 
 require File.dirname(__FILE__) + '/../../spec_helper'
 
-describe Sysadmin::DirExtension, 'Dir クラス拡張' do
-  context 'で filelist メソッドを呼ぶ場合' do
-    describe 'ディレクトリ名を指定すると' do
-      it "ディレクトリ内のファイル一覧が返却される" do
-        test_dir = File.expand_path("../../test_dir/a", File.dirname(__FILE__))
-        Dir.filelist(test_dir).should have(2).items
+describe Dir do
+  describe '#filelist' do
+    context 'specity directory name' do
+      subject { Dir.filelist(dir) }
+
+      let(:dir) { File.expand_path("../../test_dir/a", File.dirname(__FILE__)) }
+      it { expect(subject).to have(2).items }
+
+      context 'if subdirectory option is true' do
+        subject { Dir.filelist(dir, sub_directory = true) }
+
+        let(:dir) { File.expand_path("../../test_dir", File.dirname(__FILE__)) }
+        it { expect(subject).to have(9).items }
+      end
+
+      context 'if subdirectory option is false' do
+        subject { Dir.filelist(dir, sub_directory = false) }
+
+        let(:dir) { File.expand_path("../../test_dir/a", File.dirname(__FILE__)) }
+        it { expect(subject).to have(2).items }
       end
     end
 
-    describe 'ファイル名を指定すると' do
-      it "ファイル名が返却される" do
-        test_dir = File.expand_path("../../test_dir/a/d.txt", File.dirname(__FILE__))
-        Dir.filelist(test_dir).should have(1).items
+    context 'specity file name' do
+      subject { Dir.filelist(file) }
+
+      let(:file) { File.expand_path("../../test_dir/a/d.txt", File.dirname(__FILE__)) }
+      it { expect(subject).to have(1).item }
+
+      context 'if subdirectory option is true' do
+        subject { Dir.filelist(file, sub_directory = true) }
+
+        let(:file) { File.expand_path("../../test_dir/a/d.txt", File.dirname(__FILE__)) }
+        it { expect(subject).to have(1).item }
+      end
+
+      context 'if subdirectory option is true' do
+        subject { Dir.filelist(file, sub_directory = false) }
+
+        let(:file) { File.expand_path("../../test_dir/a/d.txt", File.dirname(__FILE__)) }
+        it { expect(subject).to have(1).item }
       end
     end
-
-    describe 'サブディレクトリを true にしてディレクトリを指定すると' do
-      it "サブディレクトリ内のファイルも返却される" do
-        test_dir = File.expand_path("../../test_dir", File.dirname(__FILE__))
-        Dir.filelist(test_dir, sub_directory = true).should have(9).items
-      end
-    end
-
-    describe 'サブディレクトリを false にしてディレクトリを指定すると' do
-      it "ディレクトリ内のファイル一覧が返却される (サブディレクトリは見ない)" do
-        test_dir = File.expand_path("../../test_dir/a", File.dirname(__FILE__))
-        Dir.filelist(test_dir, sub_directory = false).should have(2).items
-      end
-    end
-
-    describe 'サブディレクトリを true にしてファイル名を指定しても' do
-      it "ファイル名が返却される (サブディレクトリは見ない)" do
-        test_dir = File.expand_path("../../test_dir/a/d.txt", File.dirname(__FILE__))
-        Dir.filelist(test_dir).should have(1).items
-      end
-    end
-
   end
 end
