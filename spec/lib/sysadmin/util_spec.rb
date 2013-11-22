0f6fa1cdddddd09940ee761725ae22be808710a5
id774 <idnanashi@gmail.com>
2013-11-22 15:42:19 +0900

    46 Improvement specs

--- spec/lib/sysadmin/util_spec.rb
+++ spec/lib/sysadmin/util_spec.rb
@@ -3,14 +3,15 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
 describe Sysadmin::Util do
-  context 'で create_multi_dimensional_hash メソッドを呼ぶ場合' do
-    describe '引数を付けずに呼ぶと' do
-      it "多次元ハッシュが返る" do
+  describe '#create_multi_dimensional_hash' do
+    context 'with no arguments' do
+      subject {
         h = Sysadmin::Util.create_multi_dimensional_hash
         h["a"]["b"]["c"]["d"] = 10
-        h["a"]["b"]["c"]["e"] = 15
-        h["a"]["b"]["c"]["d"].should == 10
-        h["a"]["b"]["c"]["e"].should == 15
+      }
+
+      it 'should be returned multi dimensional hash' do
+        expect(subject).to eql 10
       end
     end
   end
