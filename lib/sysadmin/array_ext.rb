cdc4d28799200f3ddc4df9eac8443c345e5ec0b2
id774 <idnanashi@gmail.com>
2013-11-22 10:06:52 +0900

    44 Add extension Array#sum, Array#sumf methods

--- lib/sysadmin/array_ext.rb
+++ lib/sysadmin/array_ext.rb
@@ -1,11 +1,19 @@
 # Name::      Sysadmin::ArrayExtension
 # Author::    774 <http://id774.net>
 # Created::   Aug 20, 2013
-# Updated::   Oct 08, 2013
+# Updated::   Nov 21, 2013
 # Copyright:: 774 Copyright (c) 2013
 # License::   Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3.0.
 
 class Array
+
+  def sum
+    inject(0){|result, item| result + item.to_i}
+  end
+
+  def sumf
+    inject(0){|result, item| result + item.to_f}
+  end
 
   def average
     inject(0.0) { |sum, i| sum += i } / size
--- spec/lib/sysadmin/array_ext_spec.rb
+++ spec/lib/sysadmin/array_ext_spec.rb
@@ -2,25 +2,73 @@
 
 require File.dirname(__FILE__) + '/../../spec_helper'
 
-describe Sysadmin, 'Array クラス拡張' do
-  context 'で average メソッドを呼ぶ場合' do
-    it "平均が返却される" do
-      a = [88, 99, 77, 66, 55, 40]
-      a.average.should == 70.83333333333333
+describe Array do
+  describe '#sum' do
+    context 'with no arguments' do
+      subject { array.sum }
+
+      context 'if all elements are integer' do
+        let(:array) { [1, 2, 3, 4, 5] }
+        it { expect(subject).to eql 15 }
+      end
+
+      context 'if array contains string' do
+        let(:array) { [1, 2, 3, '4', 5] }
+        it { expect(subject).to eql 15 }
+      end
+
+      context 'if array contains nil' do
+        let(:array) { [1, 2, 3, nil, 5] }
+        it { expect(subject).to eql 11 }
+      end
     end
   end
 
-  context 'で variance メソッドを呼ぶ場合' do
-    it "分散が返却される" do
-      a = [88, 99, 77, 66, 55, 40]
-      a.variance.should == 391.8055555555556
+  describe '#sumf' do
+    context 'with no arguments' do
+      subject { array.sumf }
+
+      context 'if all elements are integer' do
+        let(:array) { [1, 2, 3, 4, 5] }
+        it { expect(subject).to eql 15.0 }
+      end
+
+      context 'if array contains string' do
+        let(:array) { [1, 2, 3, '4.2', 5] }
+        it { expect(subject).to eql 15.2 }
+      end
+
+      context 'if array contains string' do
+        let(:array) { [1, 2, 3, nil, 5] }
+        it { expect(subject).to eql 11.0 }
+      end
     end
   end
 
-  context 'で standard_deviation メソッドを呼ぶ場合' do
-    it "標準偏差が返却される" do
-      a = [88, 99, 77, 66, 55, 40]
-      a.standard_deviation.should == 19.79407880037754
+  describe '#average' do
+    context 'with no arguments' do
+      subject { array.average }
+
+      let(:array) { [88, 99, 77, 66, 55, 40] }
+      it { expect(subject).to eql 70.83333333333333 }
+    end
+  end
+
+  describe '#variance' do
+    context 'with no arguments' do
+      subject { array.variance }
+
+      let(:array) { [88, 99, 77, 66, 55, 40] }
+      it { expect(subject).to eql 391.8055555555556 }
+    end
+  end
+
+  describe '#standard_deviation' do
+    context 'with no arguments' do
+      subject { array.standard_deviation }
+
+      let(:array) { [88, 99, 77, 66, 55, 40] }
+      it { expect(subject).to eql 19.79407880037754 }
     end
   end
 end
