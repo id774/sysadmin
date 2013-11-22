174c693f94af9d4fa3a525ccffb7fcf08b9ba086
id774 <idnanashi@gmail.com>
2013-11-22 16:34:24 +0900

    49 Improvement spec console output

--- spec/lib/sysadmin/array_ext_spec.rb
+++ spec/lib/sysadmin/array_ext_spec.rb
@@ -9,17 +9,23 @@ describe Array do
 
       context 'if all elements are integer' do
         let(:array) { [1, 2, 3, 4, 5] }
-        it { expect(subject).to eql 15 }
+        it 'should return integer summary of elements' do
+          expect(subject).to eql 15
+        end
       end
 
       context 'if array contains string' do
         let(:array) { [1, 2, 3, '4', 5] }
-        it { expect(subject).to eql 15 }
+        it 'should return integer summary of elements' do
+          expect(subject).to eql 15
+        end
       end
 
       context 'if array contains nil' do
         let(:array) { [1, 2, 3, nil, 5] }
-        it { expect(subject).to eql 11 }
+        it 'should return integer summary of elements' do
+          expect(subject).to eql 11
+        end
       end
     end
   end
@@ -30,17 +36,23 @@ describe Array do
 
       context 'if all elements are integer' do
         let(:array) { [1, 2, 3, 4, 5] }
-        it { expect(subject).to eql 15.0 }
+        it 'should return float summary of elements' do
+          expect(subject).to eql 15.0
+        end
       end
 
       context 'if array contains string' do
         let(:array) { [1, 2, 3, '4.2', 5] }
-        it { expect(subject).to eql 15.2 }
+        it 'should return float summary of elements' do
+          expect(subject).to eql 15.2
+        end
       end
 
       context 'if array contains string' do
         let(:array) { [1, 2, 3, nil, 5] }
-        it { expect(subject).to eql 11.0 }
+        it 'should return float summary of elements' do
+          expect(subject).to eql 11.0
+        end
       end
     end
   end
@@ -49,8 +61,10 @@ describe Array do
     context 'with no arguments' do
       subject { array.average }
 
-      let(:array) { [88, 99, 77, 66, 55, 40] }
-      it { expect(subject).to eql 70.83333333333333 }
+      let(:array) { [8, 9, 7, 6, 5, 4] }
+      it 'should return average of array' do
+        expect(subject).to eql 6.5
+      end
     end
   end
 
@@ -58,8 +72,10 @@ describe Array do
     context 'with no arguments' do
       subject { array.variance }
 
-      let(:array) { [88, 99, 77, 66, 55, 40] }
-      it { expect(subject).to eql 391.8055555555556 }
+      let(:array) { [8, 9, 7, 6, 5, 4] }
+      it 'should return variance of array' do
+        expect(subject).to eql 2.9166666666666665
+      end
     end
   end
 
@@ -67,8 +83,10 @@ describe Array do
     context 'with no arguments' do
       subject { array.standard_deviation }
 
-      let(:array) { [88, 99, 77, 66, 55, 40] }
-      it { expect(subject).to eql 19.79407880037754 }
+      let(:array) { [8, 9, 7, 6, 5, 4] }
+      it 'should return standard deviation of array' do
+        expect(subject).to eql 1.707825127659933
+      end
     end
   end
 end
