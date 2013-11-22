94603618822f909fd1fba940b48dcc6ef0e71988
id774 <idnanashi@gmail.com>
2013-11-22 11:26:20 +0900

    46 Improvement specs

--- spec/lib/sysadmin/dir_ext_spec.rb
+++ spec/lib/sysadmin/dir_ext_spec.rb
@@ -8,20 +8,26 @@ describe Dir do
       subject { Dir.filelist(dir) }
 
       let(:dir) { File.expand_path("../../test_dir/a", File.dirname(__FILE__)) }
-      it { expect(subject).to have(2).items }
+      it 'should return file list in directories' do
+        expect(subject).to have(2).items
+      end
 
       context 'if subdirectory option is true' do
         subject { Dir.filelist(dir, sub_directory = true) }
 
         let(:dir) { File.expand_path("../../test_dir", File.dirname(__FILE__)) }
-        it { expect(subject).to have(9).items }
+        it 'should return file name (not in sub directories)' do
+         expect(subject).to have(9).items
+        end
       end
 
       context 'if subdirectory option is false' do
         subject { Dir.filelist(dir, sub_directory = false) }
 
         let(:dir) { File.expand_path("../../test_dir/a", File.dirname(__FILE__)) }
-        it { expect(subject).to have(2).items }
+        it 'should return file name not including sub directories' do
+          expect(subject).to have(2).items
+        end
       end
     end
 
@@ -29,20 +35,26 @@ describe Dir do
       subject { Dir.filelist(file) }
 
       let(:file) { File.expand_path("../../test_dir/a/d.txt", File.dirname(__FILE__)) }
-      it { expect(subject).to have(1).item }
+      it 'should return the file name' do
+        expect(subject).to have(1).item
+      end
 
       context 'if subdirectory option is true' do
         subject { Dir.filelist(file, sub_directory = true) }
 
         let(:file) { File.expand_path("../../test_dir/a/d.txt", File.dirname(__FILE__)) }
-        it { expect(subject).to have(1).item }
+        it 'should return file list including subdirectories' do
+          expect(subject).to have(1).item
+        end
       end
 
       context 'if subdirectory option is true' do
         subject { Dir.filelist(file, sub_directory = false) }
 
         let(:file) { File.expand_path("../../test_dir/a/d.txt", File.dirname(__FILE__)) }
-        it { expect(subject).to have(1).item }
+        it 'should return file list not including subdirectories' do
+          expect(subject).to have(1).item
+        end
       end
     end
   end
